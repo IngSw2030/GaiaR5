8e0d1aa807a97290c809c206a85e659e47b37e5c
gomezan <36377557+gomezan@users.noreply.github.com>
2020-10-15 15:51:22 -0500

    ruta simple
    realiza un poligono entre dos ubicaciones

--- GaiaR5Cliente/src/components/mapa.vue
+++ GaiaR5Cliente/src/components/mapa.vue
@@ -2,25 +2,21 @@
 
   <q-page>
 
-    <h2>  quiero saber mis coordenadas   </h2>
+    <h2>  quiero saber mis coordenadas !!!!  </h2>
 
+    <p>  {{coordenadas.lat}} latitud, {{coordenadas.lng}} longitud,  </p>
 
     <GmapMap
-      :center="{lat:4.629070, lng:-74.062716}"
+      :center="origen"
       :zoom="15"
       map-type-id="roadmap"
       style="width: 500px; height: 300px"
     >
-      //<GmapMarker
-        //:key="index"
-        //v-for="(m, index) in markers"
-        //:position="m.position"
-        //:clickable="true"
-        //:draggable="true"
-        //@click="center=m.position"
-      ///>
 
-      <GmapMarker :position="{lat:  4.629070, lng: -74.062716}" />
+      <GmapMarker :position="origen" />
+      <GmapMarker :position="{lat:  destino.lat, lng: destino.lng}" />
+
+      <gmap-polygon :paths="[origen,destino]" :editable="false" :draggable="false" :path="true" ></gmap-polygon>
 
     </GmapMap>
 
@@ -32,14 +28,39 @@
 import Vue from 'vue'
 import * as VueGoogleMaps from 'vue2-google-maps'
 
+
 Vue.use(VueGoogleMaps, {
   load: {
-    key: 'AIzaSyB5GlEzO74zRRZaPcWeWG4nNdPDUeTdXro'
+    key: 'AIzaSyCvJo0DJMLi_DQFL_nw_XC0M2LFzU-YK40'
   }
 })
 
- export default {
 
+
+
+ export default {
+ data(){
+      return{
+        coordenadas:{
+          lat:0,
+          lng:0
+        },
+
+        origen:{
+          lat:4.629070,
+          lng: -74.062716
+        },
+
+        destino:{
+          lat:4.632304,
+          lng:-74.070685
+        }
+
+      }
+
+ },
+   created(){
+   }
 }
 </script>
 
